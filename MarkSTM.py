8fc71b2223e593ef4328ae7996a83613a33b79e4
Ryan Rudd <ry.an.ruddd@gmail.com>
2023-04-28 07:45:17 -0400

    Get historic stock data

--- MarkSTM.py
+++ MarkSTM.py
@@ -1,4 +1,20 @@
 from api import __api_init__
+import matplotlib.pyplot as plt
+from datetime import datetime
+import yfinance as yf
+from pandas.core.frame import DataFrame
+def benchmark():
+    __api_init__.API.Backtest()
+    __api_init__.API.Evaluate()
+    __api_init__.API.Metrics()
 
-__api_init__.API.Backtest()
-__api_init__.API.Evaluate()
+def getHistoric(stock) -> DataFrame:
+    today_date = datetime.today()
+    start_date = datetime(1, 1, 1)
+    end_date = datetime(today_date.year, today_date.month, today_date.day)
+    print(today_date.day)
+    data = yf.download(f'{stock}')
+    return data
+    
+ticker = input("What ticker do you want to trade:\t")
+print(getHistoric(ticker)['Open'])
